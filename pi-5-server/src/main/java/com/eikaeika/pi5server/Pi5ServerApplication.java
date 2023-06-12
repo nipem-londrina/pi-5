ea5f06249806e1dd74b822d974bd5c6013a7b5f5
erich-ika <erich.sabe.tudo@gmail.com>
2023-06-12 20:06:52 -0300

    Projetos criados

---
+++ pi-5-server/src/main/java/com/eikaeika/pi5server/Pi5ServerApplication.java
@@ -0,0 +1,13 @@
+package com.eikaeika.pi5server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi5ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pi5ServerApplication.class, args);
+    }
+
+}
---
+++ pi-5-server/src/test/java/com/eikaeika/pi5server/Pi5ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eikaeika.pi5server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi5ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
