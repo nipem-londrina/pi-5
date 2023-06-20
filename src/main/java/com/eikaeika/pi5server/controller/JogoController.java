b821c8ca9152542da5d8445222da0396adb8ff63
erich-ika <erich.sabe.tudo@gmail.com>
2023-06-19 22:32:18 -0300

    Update JogoController.java

--- src/main/java/com/eikaeika/pi5server/controller/JogoController.java
+++ src/main/java/com/eikaeika/pi5server/controller/JogoController.java
@@ -3,22 +3,17 @@ package com.eikaeika.pi5server.controller;
 import com.eikaeika.pi5server.model.Jogo;
 import com.eikaeika.pi5server.repository.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/jogo")
 public class JogoController {
     @Autowired
     public JogoRepository repository;
 
-    @GetMapping("/api/v1/jogador")
-    Iterable<Jogo> findAll() {
-        return repository.findByOrderByNome();
-    }
-
-    @GetMapping("/api/v1/jogador/search/{nome}")
-    Iterable<Jogo> search(@PathVariable String nome) {
+    @GetMapping
+    Iterable<Jogo> find(@RequestParam(required = false) String nome) {
+        if (nome == null) return repository.findByOrderByNome();
         return repository.findByNomeContainingIgnoreCaseOrderByNome(nome);
     }
 }
